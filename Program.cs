5032e00a4b30d1240074eb55474afd93ca54f273
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-02 15:05:40 +0300

    LinkedList solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿// each question have own branch
+﻿class MinStack
+{
+    private readonly LinkedList<int> _stack;
+    private readonly LinkedList<int> _minStack;
+    public MinStack()
+    {
+        _stack = new LinkedList<int>();
+        _minStack = new LinkedList<int>();
+    }
+
+    public void Push(int val)
+    {
+        if (_minStack.Count == 0)
+        {
+            _minStack.AddFirst(val);
+        }
+        else
+        {
+            var min = _minStack.First.Value;
+            if(min >= val) _minStack.AddFirst(val);
+        }
+        _stack.AddFirst(val);
+    }
+
+    public void Pop()
+    {
+        var val = _stack.First.Value;
+        if(_minStack.First.Value == val) _minStack.RemoveFirst();
+        _stack.RemoveFirst();
+    }
+
+    public int Top()
+    {
+        return _stack.First.Value;
+    }
+
+    public int GetMin()
+    {
+        return _minStack.First.Value;
+    }
+}
