92fe8f34f3f8af411ed5e0148910fe499cb82c35
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-03 21:27:31 +0300

    solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
 ﻿// each question have own branch
+int DiameterOfBinaryTree(TreeNode root)
+{
+    int max = 0;
+    int BFS(TreeNode root)
+    {
+        if (root is null) return -1;
+        int l = BFS(root.left);
+        int r = BFS(root.right);
+        max = Math.Max(max, 2 + l + r);
+        return (l > r ? l : r) + 1;
+    }
+
+    BFS(root);
+    return max;
+}
+
+public class TreeNode
+{
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) 
+     {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+     }
+}
