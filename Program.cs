764b5c33b58a6ce4bc287f57c96d2c3aaf997ac9
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-03 20:29:25 +0300

    solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿// each question have own branch
+int MaxProfit(int[] prices)
+{
+    int l = 0, r = 1, maxProfit = 0;
+    for (int i=1;i < prices.Length;i++)
+    {
+        int profit = prices[r] - prices[l];
+        if (profit > maxProfit) maxProfit = profit;
+        if (prices[r] < prices[l])
+        {
+            l = r;
+        }
+        r++;
+    }
+    return maxProfit;
+}
