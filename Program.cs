66a908ffa20b9d187b83d837fc8fa3bd8e2dd6f6
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-11 18:55:45 +0300

    minor fix

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ IList<IList<int>> ThreeSum(int[] nums)
     var res = new List<IList<int>>();
     Array.Sort(nums);
     int prev = -1;
-    for (int i = 0; i < nums.Length - 2; i++)
+    for (int i = 0; i < nums.Length; i++)
     {
         if (i > 0 && prev == nums[i]) continue;
         int first = nums[i];
-        int l = i + 1, r = nums.Length;
+        int l = i + 1, r = nums.Length - 1;
 
         while (l < r)
         {
@@ -20,9 +20,10 @@ IList<IList<int>> ThreeSum(int[] nums)
             {
                 res.Add(new List<int>() { first, nums[l], nums[r] });
                 l++;
-                while (nums[l-1] == nums[l] && l < r) l++;
+                while (nums[l - 1] == nums[l] && l < r) l++;
             }
         }
+        prev = nums[i];
     }
 
     return res;
