164840eda9adc34054c7943bd30c35802b80409c
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-12 22:50:26 +0300

    100. Same Tree

--- Program.cs
+++ Program.cs
@@ -1,29 +1,19 @@
 ﻿// each question have own branch
 
-IList<IList<int>> ThreeSum(int[] nums)
+bool IsSameTree(TreeNode p, TreeNode q)
 {
-    var res = new List<IList<int>>();
-    Array.Sort(nums);
-    int prev = -1;
-    for (int i = 0; i < nums.Length - 2; i++)
+
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
     {
-        if (i > 0 && prev == nums[i]) continue;
-        int first = nums[i];
-        int l = i + 1, r = nums.Length;
-
-        while (l < r)
-        {
-            int sum = first + nums[l] + nums[r];
-            if (sum > 0) r--;
-            else if (sum < 0) l++;
-            else
-            {
-                res.Add(new List<int>() { first, nums[l], nums[r] });
-                l++;
-                while (nums[l-1] == nums[l] && l < r) l++;
-            }
-        }
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
-
-    return res;
 }
