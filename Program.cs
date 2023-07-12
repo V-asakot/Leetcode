7211af12a3c72a2e0e8ab8652e9c51074f4a3247
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-12 22:50:52 +0300

    solution

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 
 bool IsSameTree(TreeNode p, TreeNode q)
 {
+    if (p is null && q is null) return true;
+    else if (p is null || q is null || p.val != q.val) return false;
 
+    return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
 }
 
 public class TreeNode
