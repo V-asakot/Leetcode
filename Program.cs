12fc556546e5d2e1b4fef3f014a34bf0ffa89566
Vasakot <vasily.kotkoff@yandex.ru>
2023-07-24 00:15:19 +0300

    46. Permutations

--- Program.cs
+++ Program.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 
 IList<IList<int>> Permute(int[] nums)
 {
-    var res = new List<IList<int>>();
-    var current = new LinkedList<int>();
-    void DFS(List<int> nums)
-    {
-        if (nums.Count == 0)
+        var res = new List<IList<int>>();
+        var current = new List<int>();
+        void DFS(List<int> nums)
         {
-            res.Add(new List<int>(current));
-            return;
+            if (nums.Count == 0) 
+            {
+                res.Add(new List<int>(current));
+                return; 
+            }
+
+            for (int i=0;i < nums.Count; i++)
+            {
+                int currentNum = nums[i];
+
+                current.Add(currentNum);
+                nums.RemoveAt(i);
+
+                DFS(nums);
+                current.RemoveAt(current.Count-1);
+                nums.Insert(i,currentNum);
+            }
+
         }
-
-        for (int i = 0; i < nums.Count; i++)
-        {
-            int currentNum = nums[i];
-
-            current.AddLast(currentNum);
-            nums.RemoveAt(i);
-
-            DFS(nums);
-            current.RemoveLast();
-            nums.Insert(i, currentNum);
-        }
-
-    }
-    DFS(nums.ToList());
-    return res;
+        DFS(nums.ToList());
+        return res;
 }
