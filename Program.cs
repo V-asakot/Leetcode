89ef8c7380acbca980a4f574f4d97b3741e64cdf
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-03 22:19:00 +0300

    200. Number of Islands

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 ﻿// each question have own branch
 
-int ClimbStairs(int n)
+var array = new char[][] { new char[] { '1', '1', '1', '1', '0' }, new char[] { '1', '1', '0', '1', '0' }, new char[] { '1', '1', '0', '0', '0' }, new char[] { '0', '0', '0', '0', '0' } };
+NumIslands(array);
+int NumIslands(char[][] grid)
 {
-    if (n <= 2) return n;
-    int first = 1, second = 2;
-    for (int i=2;i<n;i++)
+    int result = 0;
+    int h = grid.Length; 
+    int w = grid[0].Length;
+
+    void bfs(int i,int j)
     {
-        int sum = first + second;
-        first = second;
-        second = sum;
+        var q = new Queue<(int x, int y)>();
+        q.Enqueue((i,j));
+        var directions = new (int x, int y)[] { (1,0),(-1,0), (0, 1), (0, -1) };
+        while (q.Count > 0) 
+        {
+            var cur = q.Dequeue();
+            foreach (var direction in directions)
+            {
+                var pos = (x:cur.x + direction.x, y:cur.y + direction.y);
+                if (pos.x < h && pos.x >= 0 && pos.y < w && pos.y >= 0 && grid[pos.x][pos.y] == '1') 
+                {
+                    q.Enqueue(pos);
+                    grid[pos.x][pos.y] = '2';
+                }
+            }
+        }
     }
-    return second;
+
+    for (int i=0;i<h;i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                bfs(i,j);
+                result++;
+            }
+        }
+    }
+    return result;
 }
