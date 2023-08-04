b8151fc61c27246ff9adb821480c99ac3855a7f9
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-04 20:12:31 +0300

    19.Remove Nth Node From End of List

--- Program.cs
+++ Program.cs
@@ -1,43 +1,33 @@
 ﻿// each question have own branch
 
-var array = new char[][] { new char[] { '1', '1', '1', '1', '0' }, new char[] { '1', '1', '0', '1', '0' }, new char[] { '1', '1', '0', '0', '0' }, new char[] { '0', '0', '0', '0', '0' } };
-NumIslands(array);
-int NumIslands(char[][] grid)
+ListNode RemoveNthFromEnd(ListNode head, int n)
 {
-    int result = 0;
-    int h = grid.Length; 
-    int w = grid[0].Length;
-
-    void bfs(int i,int j)
+    var dummy = new ListNode(next: head);
+    var slow = dummy;
+    var fast = head;
+    while (n > 0 && fast != null)
     {
-        var q = new Queue<(int x, int y)>();
-        q.Enqueue((i,j));
-        var directions = new (int x, int y)[] { (1,0),(-1,0), (0, 1), (0, -1) };
-        while (q.Count > 0) 
-        {
-            var cur = q.Dequeue();
-            foreach (var direction in directions)
-            {
-                var pos = (x:cur.x + direction.x, y:cur.y + direction.y);
-                if (pos.x < h && pos.x >= 0 && pos.y < w && pos.y >= 0 && grid[pos.x][pos.y] == '1') 
-                {
-                    q.Enqueue(pos);
-                    grid[pos.x][pos.y] = '2';
-                }
-            }
-        }
+        fast = fast.next;
+        n--; 
     }
 
-    for (int i=0;i<h;i++)
+    while (fast != null)
     {
-        for (int j = 0; j < w; j++)
-        {
-            if (grid[i][j] == '1')
-            {
-                bfs(i,j);
-                result++;
-            }
-        }
+        fast = fast.next;
+        slow = slow.next;
     }
-    return result;
+
+    slow.next = slow.next.next;
+    return dummy.next;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
 }
