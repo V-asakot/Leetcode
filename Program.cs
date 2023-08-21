cf9b9bdd256bd9e861bdb64236868d8761e08a01
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-21 22:56:44 +0300

    739. Daily Temperatures

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 ﻿// each question have own branch
-int[] ProductExceptSelf(int[] nums)
+int[] DailyTemperatures(int[] temperatures)
 {
-    var output = new int[nums.Length];
-    int prefix = 1;
-    for (int i = 0;i < nums.Length;i++)
+    var stack = new Stack<(int,int)>();
+    stack.Push((0,temperatures[0]));
+    temperatures[0] = 0;
+    for (int i=1;i < temperatures.Length;i++)
     {
-        output[i] = prefix;
-        prefix *= nums[i];
+        int val = temperatures[i];
+        while (stack.Any() && val > stack.Peek().Item2)
+        {
+            var current = stack.Pop();
+            temperatures[current.Item1] = i - current.Item1;
+        }
+        temperatures[i] = 0;
+        stack.Push((i,val));
     }
-    int postfix = 1;
-    for (int i = nums.Length; i >= 0; i--)
-    {
-        output[i] *= postfix;
-        prefix *= nums[i];
-    }
-    return output;
+    return temperatures;
 }
