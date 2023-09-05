aa95df7de7d021f98d92993d93b123b834bdd3bf
Vasakot <vasily.kotkoff@yandex.ru>
2023-09-05 22:16:38 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ IList<IList<int>> SubsetsWithDup(int[] nums)
     int length = nums.Length;
     Array.Sort(nums);
     var indexes = new Dictionary<int, int>();
-    for (int i = 0; i < length - 1; i++)
+    for (int i = 0; i < length; i++)
     {
-        if(nums[i] != nums[i+1]) indexes[nums[i]] = i;
+        indexes[nums[i]] = i;
     }
 
     IList<IList<int>> res = new List<IList<int>>();
