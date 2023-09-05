1ba7fa2ad9b645a28ddb3975d5fdda7e1b9dc71a
Vasakot <vasily.kotkoff@yandex.ru>
2023-09-05 22:16:06 +0300

    90. Subsets II better solution

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ IList<IList<int>> SubsetsWithDup(int[] nums)
 {
     int length = nums.Length;
     Array.Sort(nums);
+    var indexes = new Dictionary<int, int>();
+    for (int i = 0; i < length - 1; i++)
+    {
+        if(nums[i] != nums[i+1]) indexes[nums[i]] = i;
+    }
+
     IList<IList<int>> res = new List<IList<int>>();
     List<int> current = new List<int>();
     void DFS(int i)
@@ -14,16 +20,10 @@ IList<IList<int>> SubsetsWithDup(int[] nums)
             return;
         }
 
-        int curr = nums[i];
-        current.Add(curr);
-        int step = i + 1;  
-        DFS(step);
+        current.Add(nums[i]);
+        DFS(i + 1);
         current.RemoveAt(current.Count() - 1);
-        while (step < length && nums[step] == curr)
-        {
-            step++;
-        }
-        DFS(step);
+        DFS(indexes[nums[i]] + 1);
     }
     DFS(0);
     return res;
