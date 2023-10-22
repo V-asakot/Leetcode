ff76bc1331a7ecd17329dd1b72d6e946a36844dd
Valii Kotkov <vasily.kotkoff@yandex.ru>
2023-10-22 21:57:56 +0300

    FindDuplicate

--- Program.cs
+++ Program.cs
@@ -1,39 +1,18 @@
-﻿using System;
-using System.Text;
-
-bool Exist(char[][] board, string word) 
-{
-    int rows = board.Length, columns = board[0].Length;
-    var set = new HashSet<(int,int)>();
-
-    bool DFS(int r,int c ,int i)
+﻿int FindDuplicate(int[] nums) {
+    int slow = 0, fast = 0;
+    while(true)
     {
-        if(i == word.Length)
-        {
-            return true;
-        }
-
-        if(r < 0 || c <  0 || r >= rows || c >= columns || word[i] != board[r][c] || set.Contains((r,c)))
-        {
-            return false;
-        }
-
-        set!.Add((r,c));
-        var res  = DFS(r+1,c ,i+1) || DFS(r-1,c ,i+1) || DFS(r,c+1 ,i+1) || DFS(r,c-1 ,i+1);
-        set!.Remove((r,c));
-        return res;
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if(slow == fast) break;
     }
 
-    for(int i=0; i < rows ;i++)
-    {
-        for(int j=0; j < columns ;j++)
-        {
-            if(DFS(i,j,0))
-            {
-                return true;
-            }
-        }
-    }
+    int slow_ = 0;
 
-    return false;
+    while(true)
+    {
+        slow = nums[slow];
+        slow_ = nums[slow_];
+        if(slow == slow_) return slow_;
+    }
 }
