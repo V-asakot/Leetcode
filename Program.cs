251f8af59e5d26d18866b8e48ec98228d2aca1d4
Valii Kotkov <vasily.kotkoff@yandex.ru>
2023-11-25 19:46:16 +0300

    567. Permutation in String

--- Program.cs
+++ Program.cs
@@ -1,18 +1,65 @@
-﻿int FindDuplicate(int[] nums) {
-    int slow = 0, fast = 0;
-    while(true)
+﻿using System.Collections;
+
+bool CheckInclusion(string s1, string s2) 
     {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-        if(slow == fast) break;
+                 int l1 = s1.Length;
+            int l2 = s2.Length;
+            if(l1 > l2)
+            {
+                return false;
+            }
+            var h1 = new int[26];
+            var h2 = new int[26];
+            int matches = 0;
+
+            foreach(char ch in s1)
+            {
+                h1[Index(ch)]++;
+                h2[Index(ch)]++;
+            }     
+
+            for(int i=0;i<26;i++)
+            {
+                if(h1[i] == h2[i])
+                {
+                    matches++;
+                }
+            }
+
+            int j = 0;
+            for(int i = l1;i < l2;i++)
+            {  
+                if(matches == 26)return true;
+
+                var index = Index(s2[i]);
+                h2[index]++;
+                if(h1[index] == h2[index])
+                {
+                    matches++;
+                }
+                else if(h1[index] + 1 == h2[index])
+                {
+                    matches--;
+                }
+
+
+                index = Index(s2[j]);
+                h2[index]--;
+                if(h1[index] == h2[index])
+                {
+                    matches++;
+                }
+                else if(h1[index]  - 1 == h2[index])
+                {
+                    matches--;
+                }
+
+                j++;
+            }
+
+            return matches == 26;
     }
 
-    int slow_ = 0;
+    int Index(int ch) => ch - 97;
 
-    while(true)
-    {
-        slow = nums[slow];
-        slow_ = nums[slow_];
-        if(slow == slow_) return slow_;
-    }
-}
+Console.WriteLine(CheckInclusion("ab","eidbaooo"));
