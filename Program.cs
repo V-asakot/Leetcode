38c8d1e6f1eda09cfd4c8dd8c3116d88d41d24e1
Valii Kotkov <vasily.kotkoff@yandex.ru>
2023-11-27 20:48:43 +0300

    202. Happy Number

--- Program.cs
+++ Program.cs
@@ -1,65 +1,24 @@
-﻿using System.Collections;
-
-bool CheckInclusion(string s1, string s2) 
+﻿bool IsHappy(int n) 
+{
+    var hash = new HashSet<int>();
+    while(!hash.Contains(n))
     {
-                 int l1 = s1.Length;
-            int l2 = s2.Length;
-            if(l1 > l2)
-            {
-                return false;
-            }
-            var h1 = new int[26];
-            var h2 = new int[26];
-            int matches = 0;
-
-            foreach(char ch in s1)
-            {
-                h1[Index(ch)]++;
-                h2[Index(ch)]++;
-            }     
-
-            for(int i=0;i<26;i++)
-            {
-                if(h1[i] == h2[i])
-                {
-                    matches++;
-                }
-            }
-
-            int j = 0;
-            for(int i = l1;i < l2;i++)
-            {  
-                if(matches == 26)return true;
-
-                var index = Index(s2[i]);
-                h2[index]++;
-                if(h1[index] == h2[index])
-                {
-                    matches++;
-                }
-                else if(h1[index] + 1 == h2[index])
-                {
-                    matches--;
-                }
-
-
-                index = Index(s2[j]);
-                h2[index]--;
-                if(h1[index] == h2[index])
-                {
-                    matches++;
-                }
-                else if(h1[index]  - 1 == h2[index])
-                {
-                    matches--;
-                }
-
-                j++;
-            }
-
-            return matches == 26;
+        if(n == 1)
+        {
+            return true;
+        }
+        hash.Add(n);
+        
+        var sum = 0;
+        while(n > 0)
+        {
+            var temp = n % 10;
+            sum += temp * temp;
+            n /= 10;
+        }
+        n = sum;
     }
 
-    int Index(int ch) => ch - 97;
+    return false;
+}
 
-Console.WriteLine(CheckInclusion("ab","eidbaooo"));
