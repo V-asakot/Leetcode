cc5e22082a003cfc35de64aacd8cf2a495a25f98
Valii Kotkov <vasily.kotkoff@yandex.ru>
2024-10-05 00:34:52 +0300

    301. Remove Invalid Parentheses

--- Program.cs
+++ Program.cs
@@ -1,29 +1,52 @@
-﻿
-public class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
- 
-public class Solution
+﻿IList<string> RemoveInvalidParentheses(string s)
 {
-    private int GoodNode(TreeNode root, int max) 
+    int longestString = -1;
+    int length = s.Length;  
+    var res = new HashSet<string>();
+
+    DFS(0, "", 0, 0);
+
+    void DFS(int index, string str, int l, int r) 
     {
-        if (root is null) return 0;
-        var result = root.val >= max ? 1 : 0;
-        max = max > root.val ? max : root.val;
-        result += GoodNode(root.right, max);
-        result += GoodNode(root.left, max);
-        return result;
+        if (index == length)
+        {
+            if (l == r)
+            {
+                if (str.Length > longestString)
+                {
+                    longestString = str.Length;
+                    res = new HashSet<string>() { str.ToString() };
+                }
+                else if (str.Length == longestString)
+                {
+                    res.Add(str.ToString());
+                }
+            }
+        }
+        else
+        {
+            var cur = s[index];
+            if (cur == '(')
+            {
+                DFS(index + 1, str + cur, l + 1, r);
+
+                DFS(index + 1, str, l, r);
+            }
+            else if (cur == ')') 
+            {
+                DFS(index + 1, str, l, r);
+
+                if (r < l) 
+                {
+                    DFS(index + 1, str + cur, l, r + 1);
+                }
+            }
+            else
+            {
+                DFS(index + 1, str + cur, l, r);
+            }
+        }
     }
 
-    public int GoodNodes(TreeNode root)
-    {
-         return 1 + GoodNode(root.left, root.val) + GoodNode(root.right, root.val);
-    }
+    return res.ToList();
 }
